7155651326ebf444bfc04cffcf88d95375d354c5
vladcranga <vladcranga@yahoo.com>
2024-12-19 19:01:49 +0200

    Switch from Tailwind CSS CDN to local, restructure the project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {
+        animation: {
+            fadeIn: "fadeIn 2s ease-in-out",
+        },
+        keyframes: {
+            fadeIn: {
+                from: { opacity: "0" },
+                to: { opacity: "1" },
+            },
+        },
+    },
+},
+  plugins: [],
+}
+
